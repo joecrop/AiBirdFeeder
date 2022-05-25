bef92f6b5c9e8d3510678674532f3ee4b0d56c50
Joe Crop <joe.crop@gmail.com>
2022-05-25 16:41:38 -0700

    deleted unneeded scripts

--- ai8x-tools/ai8x-training/scripts/train_birdfeeder.sh
+++ ai8x-tools/ai8x-training/scripts/train_birdfeeder.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 python train.py --epochs 250 --optimizer Adam --lr 0.001 --wd 0 --deterministic --compress policies/schedule-birdfeeder.yaml --model ai85cdnet --dataset birdfeeder --confusion --param-hist --embedding --device MAX78000 "$@"
+
+OLDEST_LOG=`ls -tr logs | tail -n 1`
+cp $OLDEST_LOG/best.pth.tar ../ai8x-synthesis/trained/ai85-birdfeeder.pth.tar
+cp $OLDEST_LOG/qat_best.pth.tar ../ai8x-synthesis/trained/ai85-birdfeeder_qat.pth.tar
